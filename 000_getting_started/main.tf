33a831e64bbd0116653018f38bdd814f65e4fa09
Hatice Celik <hatice.celik@iths.se>
2025-02-09 19:56:57 +0100

    Update .gitignore to ignore Terraform plan files

--- 000_getting_started/main.tf
+++ 000_getting_started/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = ">= 5.46.0"
     }
   }
 
@@ -12,6 +12,13 @@ terraform {
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
+  alias   = "us"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-1"
+  alias   = "eu"
 }
 
 variable "instance_type" {
@@ -31,6 +38,29 @@ resource "aws_instance" "my_server" {
   }
 }
 
-output "instance_ip_addr" {
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  providers = {
+    aws = aws.eu
+  }
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+output "public_ip" {
   value = aws_instance.my_server.public_ip
 }
