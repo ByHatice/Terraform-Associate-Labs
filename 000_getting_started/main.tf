0ad22eef82091ba45d49930bac658c7ce13e4268
Hatice Celik <hatice.celik@iths.se>
2025-02-11 08:41:36 +0100

    Divide files and set up a workspace in Terraform Cloud to trigger the Terraform lifecycle via CLI

--- 000_getting_started/main.tf
+++ 000_getting_started/main.tf
@@ -1,4 +1,26 @@
+/*
+Next time, use the Terraform Cloud block configuration:
 terraform {
+  cloud {
+    organization = "example_corp"
+    hostname = "my.terraform-enterprise.host"
+    workspaces {
+      tags = ["app"]
+    }
+  }
+}
+*/
+
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "Learning_Terraform_Hatice"
+
+    workspaces {
+      name = "Terraform_example"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -9,58 +31,6 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
-  alias   = "us"
-}
-
-provider "aws" {
-  profile = "default"
-  region  = "eu-west-1"
-  alias   = "eu"
-}
-
-variable "instance_type" {
-  type = string
-}
-
 locals {
   project_name = "Hatice"
-}
-
-resource "aws_instance" "my_server" {
-  ami           = "ami-085ad6ae776d8f09c"
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "MyServer-${local.project_name}"
-  }
-}
-
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  providers = {
-    aws = aws.eu
-  }
-
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
-
-output "public_ip" {
-  value = aws_instance.my_server.public_ip
 }
